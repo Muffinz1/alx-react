38ca3eb908482efd89cccdd45d4a278ffa33eada
Muffinz1 <amiryou120@gmail.com>
2024-08-28 19:26:07 +0300

    test

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,10 +1,10 @@
 import $ from 'jquery';
-const _ = require("lodash");
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append('<p id="count">0 clicks on the button</p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
